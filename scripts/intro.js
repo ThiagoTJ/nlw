59ada881a7dd0092944ebd69bc3db4774b3d9740
ThiagoTJ <thiagogdejesus@outlook.com.br>
2021-02-07 09:45:57 -0300

    Todos os arquivos do projeto da nlw

---
+++ scripts/intro.js
@@ -0,0 +1,68 @@
+// coments
+// document.write("hello")
+
+// variaveis, tipo de dados
+// var myvar="he"
+// const myconst="he"
+
+// document.write(myconst + myvar)
+// const s1= "isso é uma string"
+// const s2= 'isso é um string'
+// const s3= `isso é um string`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1+n2)
+
+// boolean = true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objetos possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("execute uma grande logica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - Vetores são colecionadores de dados
+// const colecaoDeBolinhas = ["blue","green",1,{name:"My name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// funções - funcionalidades - Atalhos - Reuso de código
+
+// registrar função
+// function sayMyName(name){
+//   document.write(name)
+// }
+
+// // executor
+// sayMyName("Douglas")
+// sayMyName("Valesca")
+// sayMyName("Thiago")
+
+// condicionais
+
+// const notaFinal = 4
+
+// if(notaFinal < 5){
+//   document.write("Reprovado")
+// //   caminho 1
+// } else {
+//   document.write("Aprovado")
+// //   caminho 2
+// }
+
+// loop repetições
+
+// for(i = 0; i < 10; i++){
+//   document.write(`<p>${i}</p>`)
+// }
